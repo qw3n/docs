63317a24d759ffd4e8bc132311cec0e74a872c97
qw3n <qw3nv@yahoo.com>
2017-08-04 22:36:57 -0400

    A snackbar example using timeoutFn

---
+++ examples/snackbars/3.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card class="grey lighten-4">
+    <v-card-text>
+      <v-container fluid>
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-subheader
+              :style="{ color: color ? 'black' : 'blue' }"                
+              v-text="'This text can be either blue or black.'"
+            ></v-subheader>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      <v-btn block primary @click.native="snackbar = true" dark>Change text color</v-btn>
+    </v-card-text>
+    <v-snackbar
+      v-model="snackbar"
+      :timeoutFn="changeColor"
+    >
+      {{ text }}
+      <v-btn dark flat @click.native="changeColor">Yes</v-btn>
+      <v-btn dark flat @click.native="snackbar = false">No</v-btn>
+    </v-snackbar>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        snackbar: false,
+        color: true,
+        text: 'Are you sure you want to toggle text color?'
+      }
+    }
+    methods : {
+      changeColor () {
+        this.snackbar = false
+        this.color = !this.color
+      }
+    }
+  }
+</script>
