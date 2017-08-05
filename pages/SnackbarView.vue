c875307ee39e0d08c1d929b44907c17599f68143
qw3n <qw3nv@yahoo.com>
2017-08-04 22:42:31 -0400

    Added missing comma

--- pages/SnackbarView.vue
+++ pages/SnackbarView.vue
@@ -13,7 +13,7 @@
           desc: 'The <code>v-snackbar</code> component is used to display a quick message to a user. Snackbars support positioning, removal delay and callbacks.',
           examples: [
             { header: 'Position', file: 'snackbars/1', desc: 'The standard snackbar is useful for calling attention to some function that has just happened.'},
-            { header: 'Contextual', file: 'snackbars/2', desc: 'You can also contextually color the snackbar to better fit your implementation.'}
+            { header: 'Contextual', file: 'snackbars/2', desc: 'You can also contextually color the snackbar to better fit your implementation.'},
             { header: 'Timeout Function', file: 'snackbars/3', desc: 'You can use snackbars to allow users to undo a particular action. The timeout function will complete the action if the user doesn't intervene.'}
           ],
           props: {
